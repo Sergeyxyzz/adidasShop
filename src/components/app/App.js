b9efc4ad674fcb6d5831447563c606831307447c
Sergey <sergeyxyzz@yandex.ru>
2022-08-06 06:26:40 +0300

    при добавлении товара в корзину отправил объекты на сервер, при отображение товара в корзине получил данные с сервера

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,17 +3,22 @@ import Header from '../header/Header';
 import Product from '../product/Product'
 import styles from './App.module.scss'
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const [items, setItems] = useState([]) // блок получения данных с инета
   useEffect(() => {
-    fetch('https://62eb0c07705264f263d29ed6.mockapi.io/items').then(res => {
-      return res.json()
-    })
-    .then((json) => {
-      setItems(json)
+    axios.get('https://62eb0c07705264f263d29ed6.mockapi.io/items').then(res => {
+      setItems(res.data)
     })
   }, []) // конец блока получения данных
+
+  const [itemsCart, setItemsCart] = useState([]) // получили данные в корзине и отобразили их в корзине
+  useEffect(() => {
+    axios.get('https://62eb0c07705264f263d29ed6.mockapi.io/cart').then(res => {
+      setItemsCart(res.data)
+    })
+  }, []) // конец блока
 
   const [cart, setCart] = useState(false) // блок открытия/закрытия корзины по клику
   const onCart = () => {
@@ -25,17 +30,24 @@ function App() {
 
   const [cartItems, setCartItems] = useState([]) // блок добавления товара в корзину
   const onPlus = (item) => {
+    axios.post('https://62eb0c07705264f263d29ed6.mockapi.io/cart', item)
     setCartItems([...cartItems, item]) // обязательно разворачивать старый массив, затем пушим таким образом объект футболки из инета
   } // конец блока
 
+
+  const onRemoveItem = (id) => {
+    axios.delete(`https://62eb0c07705264f263d29ed6.mockapi.io/cart/${id}`)
+    itemsCart((obj) => obj.filter((items) => items.id !== id))
+  }
+
   return (<>
-    {cart ? <Cart items={cartItems} closeCart={closeCart}/> : null}
+    {cart ? <Cart items={itemsCart} closeCart={closeCart} onRemoveItem={onRemoveItem}/> : null}
     <div className={styles.wrapper}>
-      <Header onCart={onCart} closeCart={closeCart}/>
+      <Header onCart={onCart} closeCart={closeCart} />
       <h1 className={styles.title}>Футболки</h1>
       <div className={styles.wrapperProduct}>
       {items.map((obj) => {
-        return <Product 
+        return <Product
           image={obj.image} 
           price={obj.price} 
           description={obj.description}
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,14 +1,15 @@
 import styles from './Cart.module.scss'
 
-const Cart = ({closeCart, items=[]}) => {
+const Cart = ({closeCart, onRemoveItem, items=[]}) => {
     return (<div className={styles.cartWrapper}>
         <img onClick={closeCart} src="images/close.png" alt="close" className={styles.closeImg}/>
         <div className={styles.items}>
-            {items.map((obj) => (<div className={styles.item}>
+            {items.map((obj, i) => (<div className={styles.item}>
+                {obj.i}
                 <img className={styles.image} src={obj.image} alt="Футболка" />
                 <h4>{obj.description}</h4>
                 <h3>{obj.price} руб.</h3>
-                <img src="images/remove.png" alt="remove" className={styles.removeImg}/>
+                <img src="images/remove.png" onClick={() => onRemoveItem(obj.id)}  alt="remove" className={styles.removeImg}/>
                 <hr />
             </div>))}
         </div>
